fc162a197c9451e181b83019ae842e88b5293c7a
wzj <1039170164@qq.com>
2021-05-17 16:39:27 +0800

    w

--- app/src/main/java/com/example/mvvmdemo/MainActivity.java
+++ app/src/main/java/com/example/mvvmdemo/MainActivity.java
@@ -15,6 +15,7 @@ import com.example.mvvmdemo.databinding.ActivityMainBinding;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class MainActivity extends AppCompatActivity {
 
     //    TextView tvName;
